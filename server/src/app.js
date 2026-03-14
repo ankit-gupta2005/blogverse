066c34c37a3beae65dbdcdf65b31b38aca2a8541
Ankit Gupta <ankittgupta2005@gmail.com>
2026-03-14 18:46:14 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -4,23 +4,30 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const authRoutes = require("./routes/authRoutes")
-const userRoutes = require("./routes/userRoutes")
-const blogRoutes = require("./routes/blogRoutes")
-
-const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const blogRoutes = require("./routes/blogRoutes");
 const commentRoutes = require("./routes/commentRoutes");
+const connectDB = require("./config/db");
 
 const app = express();
 
 connectDB(); 
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true
+}));
+
 app.use(express.json());
-app.use("/api/auth",authRoutes)
-app.use("/api/user",userRoutes)
-app.use("/api/blog",blogRoutes)
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/blog", blogRoutes);
 app.use("/api/comment", commentRoutes);
 
+app.get("/", (req, res) => {
+    res.send("BlogVerse API is running...");
+});
 
 module.exports = app;
