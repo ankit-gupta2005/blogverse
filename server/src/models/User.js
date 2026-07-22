d92c1185b4af89f67d97288028f615d97fbe8b3e
Ankit Gupta <ankittgupta2005@gmail.com>
2026-07-22 17:30:23 +0530

    Update User.js

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,52 +1,75 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
+    profileBanner: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    // RFM Tracking Fields
+    lastLogin: {
+      type: Date,
+      default: Date.now,
+    },
+    loginCount: {
+      type: Number,
+      default: 1,
+    },
+    socialLinks: {
+      twitter: { type: String, default: "" },
+      github: { type: String, default: "" },
+      website: { type: String, default: "" },
+    },
+    savedBlogs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Blog",
+      },
+    ],
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  profileImage: {
-    type: String,
-    default: ""
-  },
-  profileBanner: {
-    type: String,
-    default: ""
-  },
-  bio: {
-    type: String,
-    default: ""
-  },
-  socialLinks: {
-    twitter: { type: String, default: "" },
-    github: { type: String, default: "" },
-    website: { type: String, default: "" }
-  },
-  savedBlogs: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Blog"
-  }],
-  followers: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
-  }],
-  following: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
-  }]
-}, {
-  timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
